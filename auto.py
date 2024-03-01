7d6b68690c251ca2a17b5deeca68818dc0c226e2
Zeltorix <alexsperrow2008@rambler.ru>
2024-03-01 23:38:31 +0300

    1

--- auto.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.system('git checkout --orphan latest_branch')
-os.system('git add -A')
-os.system('git commit -am "Фиксация"')
-os.system('git branch -D main')
-os.system('git branch -m main')
-os.system('git push -f origin main')
-print("Автоматический сброс комитов")
